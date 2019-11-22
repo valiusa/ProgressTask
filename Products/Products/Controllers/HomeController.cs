6b6ae4150330b5c29e6fbd695a05776ce8b33d27
valiusa <valentinvladimirov@abv.bg>
2019-11-22 13:30:50 +0200

    Done

---
+++ Products/Products/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Products.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Products.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<Product> p = LoadJson();
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View(p);
+        }
+
+        public static List<Product> LoadJson()
+        {
+            using (StreamReader r = new StreamReader(@"..\Products\Resources\products.json"))
+            {
+                //string json = r.ReadToEnd();
+                List<Product> items = JsonConvert.DeserializeObject<List<Product>>(r.ReadToEnd());
+
+                return items;
+            }
+        }
+    }
+}
---
+++ Products/Products/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Products.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public double UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+        public string DeliveryOn { get; set; }
+    }
+}
